40e819af4dc658f922db81ae536188d088c65983
ruslana <ruslana.prus@ukr.net>
2024-08-28 00:17:48 +0200

    benchmarks

---
+++ src/jmh/java/org/example/fibonacci/FibonacciBenchmark.java
@@ -0,0 +1,50 @@
+package org.example.fibonacci;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.concurrent.TimeUnit;
+
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Fork(value = 2, jvmArgs = {"-Xms2G", "-Xmx2G"}) // will fork the benchmark twice, with each fork running in its own JVM instance with 2GB of heap memory
+@Warmup(iterations = 3)
+@Measurement(iterations = 3)
+@State(Scope.Thread)
+public class FibonacciBenchmark {
+
+    @Param({"10", "100"})
+    private int n;
+
+    // don't use n>30 for this benchmark
+    @Benchmark
+    public BigInteger testFibonacciRecursive() {
+        FibonacciRecursive fibonacciRecursive = new FibonacciRecursive();
+        return fibonacciRecursive.solveFibonacci(n);
+    }
+
+    // can handle n=2_000_000
+    @Benchmark
+    public BigInteger testFibonacciIterative() {
+        FibonacciIterative fibonacciIterative = new FibonacciIterative();
+        return fibonacciIterative.solveFibonacci(n);
+    }
+
+    @Benchmark
+    public BigInteger testFibonacciDynamic() {
+        FibonacciDynamic fibonacciDynamic = new FibonacciDynamic();
+        return fibonacciDynamic.solveFibonacci(n);
+    }
+
+    @Benchmark
+    public BigDecimal testFibonacciMatrixExp() {
+        FibonacciMatrixExponentiation fibonacciMatrixExponentiation = new FibonacciMatrixExponentiation();
+        return fibonacciMatrixExponentiation.solveFibonacci(n);
+    }
+
+    @Benchmark
+    public BigInteger testFibonacciRecMatrixExp() {
+        FibonacciRecursiveMatrixExponentiation fibonacciRecursiveMatrixExponentiation = new FibonacciRecursiveMatrixExponentiation();
+        return fibonacciRecursiveMatrixExponentiation.solveFibonacci(n);
+    }
+}
