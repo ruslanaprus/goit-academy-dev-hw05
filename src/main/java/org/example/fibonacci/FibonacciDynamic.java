bb10762a7d2a14b2cee70b6c398d4c49c94a0022
ruslana <ruslana.prus@ukr.net>
2024-08-31 14:26:36 +0200

    comments about complexity

--- src/main/java/org/example/fibonacci/FibonacciDynamic.java
+++ src/main/java/org/example/fibonacci/FibonacciDynamic.java
@@ -8,6 +8,7 @@ import static org.example.number.Constants.MATH_CONTEXT;
 
 
 public class FibonacciDynamic implements FibonacciStrategy<BigDecimal> {
+    // due to the limited size of the map, space used by the map is constant, so space comp - O(1)
     private static final Map<Integer, BigDecimal> memo = new LinkedHashMap<Integer, BigDecimal>(8, 0.75f, true) {
         @Override
         protected boolean removeEldestEntry(Map.Entry<Integer, BigDecimal> eldest) {
@@ -18,18 +19,18 @@ public class FibonacciDynamic implements FibonacciStrategy<BigDecimal> {
     @Override
     public BigDecimal solveFibonacci(int n) {
         if (n <= 1) {
-            return BigDecimal.valueOf(n);
+            return BigDecimal.valueOf(n); // time comp - O(1)
         }
 
-        memo.put(0, BigDecimal.ZERO);
-        memo.put(1, BigDecimal.ONE);
+        memo.put(0, BigDecimal.ZERO); // time comp - O(1)
+        memo.put(1, BigDecimal.ONE); // time comp - O(1)
 
-        for (int i = 2; i <= n; i++) {
-            BigDecimal previousFib = memo.get(i - 1);
-            BigDecimal twoStepsBackFib = memo.get(i - 2);
-            memo.put(i, previousFib.add(twoStepsBackFib, MATH_CONTEXT));
+        for (int i = 2; i <= n; i++) { // time comp - O(n)
+            BigDecimal previousFib = memo.get(i - 1); // space comp - O(1)
+            BigDecimal twoStepsBackFib = memo.get(i - 2); // space comp - O(1)
+            memo.put(i, previousFib.add(twoStepsBackFib, MATH_CONTEXT)); // time comp - O(1)
         }
 
-        return memo.get(n);
+        return memo.get(n); // time comp - O(1)
     }
 }
