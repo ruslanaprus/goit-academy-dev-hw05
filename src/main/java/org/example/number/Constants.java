a3dfbce39ea2f0bc1957f674f54e0003729e2f71
ruslana <ruslana.prus@ukr.net>
2024-09-01 17:52:10 +0200

    update math context

--- src/main/java/org/example/number/Constants.java
+++ src/main/java/org/example/number/Constants.java
@@ -1,9 +1,10 @@
 package org.example.number;
 
 import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class Constants {
     public static final int MIN_VALUE = 0;
     public static final int MAX_VALUE = Integer.MAX_VALUE;
-    public static final MathContext MATH_CONTEXT = new MathContext(100);
+    public static final MathContext MATH_CONTEXT = new MathContext(105, RoundingMode.HALF_UP);
 }
